63213cd37c3a8810be8394c31d8a8982f5cdbe38
19thMarch <161609366+nvtqx1@users.noreply.github.com>
2025-04-20 20:18:49 +0700

    Add Elo_Calculation

--- Minimax.py
+++ Minimax.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 import chess
+
 
 def evaluate_board(board):
     # giá trị mỗi quân cờ
@@ -22,7 +25,8 @@ def evaluate_board(board):
                 score -= value
     return score
 
-def minimax (game, depth, isMaximizingPlayer):
+
+def minimax(game, depth, isMaximizingPlayer):
     if depth == 0:
         return -evaluate_board(game.board())
     newGameMoves = list(game.legal_moves)
@@ -40,3 +44,48 @@ def minimax (game, depth, isMaximizingPlayer):
             minEvaluation += minimax(game, depth - 1, not isMaximizingPlayer)
             game.pop()
         return minEvaluation
+
+
+def minimax_alpha_beta(game: object, depth: int, alpha: float, beta: float, isMaximizingPlayer: bool) -> int:
+    if depth == 0:
+        return -evaluate_board(game.board())
+    newGameMoves = list(game.legal_moves)
+    if isMaximizingPlayer:
+        maxEvaluation = -float('inf')
+        for move in newGameMoves:
+            game.push(move)
+            maxEvaluation = max(maxEvaluation, minimax_alpha_beta(game, depth - 1, alpha, beta, False))
+            game.pop()
+            alpha = max(alpha, maxEvaluation)
+            if beta <= alpha:
+                break
+        return maxEvaluation
+    else:
+        minEvaluation = float('inf')
+        for move in newGameMoves:
+            game.push(move)
+            minEvaluation = min(minEvaluation, minimax_alpha_beta(game, depth - 1, alpha, beta, True))
+            game.pop()
+            beta = min(beta, minEvaluation)
+            if beta <= alpha:
+                break
+        return minEvaluation
+
+
+def Elo_Calculation(rating1, rating2, result):
+    if rating2 > 2400:
+        k = 10
+    elif rating2 >= 2000 and rating2 <= 2400:
+        k = 15
+    elif rating2 >= 1600 and rating2 <= 2000:
+        k = 20
+    else:
+        k = 25
+    Qa = pow(10, rating1 / 400)
+    Qb = pow(10, rating2 / 400)
+    Ea = Qa / (Qa + Qb)
+    Eb = 1 - Ea
+    if result is True:
+        return rating1 + k * (1 - Ea)
+    else:
+        return rating2 + k * (0 - Eb)
